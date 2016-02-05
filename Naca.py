7a9788eecfc9256aa337f6cc4745a15a7f6dec61
Endryws <endmoura@poli.ufrj.br>
2016-02-05 11:18:28 -0200

    Adicionei alguns comentários e  separei a função NACA 00XX

--- Naca.py
+++ Naca.py
@@ -4,11 +4,18 @@ Created on Wed Feb 03 12:50:52 2016
 
 @author: Endryws
 """
-def Naca(c, t, x_list):
-    yt = []
-    yt_inside = []
+
+"""
+The Naca function can be found in: https://en.wikipedia.org/wiki/NACA_airfoil
+This function generates the  a simetric NACA airfoil;
+receives x_list => points between 0 and c (chord lenght) 
+          
+"""
+def Naca_00XX(c, t, x_list):
+    y_list = []
+    y_negative_list = []
     for x in x_list:
-        xc = x/c # Is just for increase speed and facilitate future changes.
+        xc= x/c # Is just for increase speed and facilitate future changes.
         a1 = 5*t*c
         t1 = 0.2969*(math.sqrt(xc))
         t2 = -0.1260*xc
@@ -16,11 +23,12 @@ def Naca(c, t, x_list):
         t4 = 0.2843*(xc**3)
         t5 = -0.1015*(xc**4)
         y = (a1*(t1+t2+t3+t4+t5))
-        y_inside = y*p
-        yt.append(y)
-        yt_inside.append(y_inside)
-    return yt,yt_inside
- 
+        y_list.append(y)
+        y_negative_list.append(y*(-1)) # is just for pick the y axis
+                                       # negative numbers
+    return y_list,y_negative_list
+    
+     
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
@@ -35,24 +43,21 @@ if __name__ == '__main__':
     
     p = 0.5 #this variabel is here to force the points fall inside the model.
         
-    xt = np.linspace(.001,c,number_of_points) # This function receives inicial
-                                          # point,finalpoin,number of therms 
-                                         #between inicial and final point
+    x_list = np.linspace(.001,c,number_of_points)  # This function receives 
+                                                   # inicial point,finalpoint,
+                                                   #number of therms between 
+                                                   # inicial and final point
     
-    yt,yt_inside = Naca(c,t,xt)
+    y_list,y_negative_list = Naca_00XX(c,t,x_list)
+    
     
     fig = plt.figure()
 
     fig.add_subplot(111)
-    plt.scatter(xt,yt)
-    plt.scatter(xt,yt_inside,c='r' )
+    plt.scatter(x_list,y_list)
+    plt.scatter(x_list,y_negative_list)
 
-    yt_neg = [ -y for y in yt] # is just for pick the negative numbers
-    yt_inside_neg = [ -y for y in yt_inside]
-    
-    plt.scatter(xt,yt_neg)
-    plt.scatter(xt,yt_inside_neg,c='r')
     
     plt.grid()
-    plt.xlabel('xt')
-    plt.ylabel('yt')
+    plt.xlabel('x values')
+    plt.ylabel('y values')
