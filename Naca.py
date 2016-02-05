d585bd244a75504b70cf2466a8134ec1fb18e87a
Endryws <endmoura@poli.ufrj.br>
2016-02-05 09:17:33 -0200

    Mostra o perfil gerado e a fração que usaremos como região utilizável para encontrar os pontos
    Modifiquei alguns comentários e adicionei os pontos internos.
    Será necessário para calcular os pontos (A,B,C,D)

--- Naca.py
+++ Naca.py
@@ -11,11 +11,12 @@ t = 0.012 # is the maximum thickness as a fraction of the chord
 number_of_points = 200
 
 
-p = 0.3 #I can´t remember how we are puting this p,
-        # but I remeber Pedro say to put.
+p = 0.5 #this variabel is here to force the points fall inside the model.
+        
 
 def Naca(c,t,xt):
     yt = []
+    yt_inside = []
     for x in xt:
         chimera = x/c # Is just for increase speed and facilitate future changes.
         a1 = 5*t*c
@@ -24,9 +25,11 @@ def Naca(c,t,xt):
         t3 = -0.3516*(chimera**2)
         t4 = 0.2843*(chimera**3)
         t5 = -0.1015*(chimera**4)
-        y = (a1*(t1+t2+t3+t4+t5))*p
+        y = (a1*(t1+t2+t3+t4+t5))
+        y_inside = y*p
         yt.append(y)
-    return yt
+        yt_inside.append(y_inside)
+    return yt,yt_inside
  
 
 if __name__ == '__main__':
@@ -38,15 +41,20 @@ if __name__ == '__main__':
                                           # point,finalpoin,number of therms 
                                          #between inicial and final point
     
-    yt = Naca(c,t,xt)
+    yt,yt_inside = Naca(c,t,xt)
     
     fig = plt.figure()
 
     fig.add_subplot(111)
     plt.scatter(xt,yt)
+    plt.scatter(xt,yt_inside,c='r' )
 
     yt_neg = [ -y for y in yt] # is just for pick the negative numbers
+    yt_inside_neg = [ -y for y in yt_inside]
+    
     plt.scatter(xt,yt_neg)
+    plt.scatter(xt,yt_inside_neg,c='r')
+    
     plt.grid()
     plt.xlabel('xt')
     plt.ylabel('yt')
