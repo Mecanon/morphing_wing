aec1951b632977d9ec11352c8746c7383857259e
eTancredo <etancredo@poli.ufrj.br>
2016-04-29 13:22:59 -0300

    changes in winglet deflection code

--- dynamic_model/deflection_winglet.py
+++ dynamic_model/deflection_winglet.py
@@ -31,43 +31,55 @@ def distance(C1,C2):
     l = np.sqrt(dx**2 + dy**2)
 
     return l
-
-c = 1.0
+#dimensions obtained from the experimental model, in centimeters
+c = 51.5
 # Defining the positions
 # Front attachment point
 F = {'x':c*0, 'y':0.}
 # Midle attachment point
-M = {'x':c*0.3, 'y':0}
+M = {'x':c*0.214, 'y':0}
 # Tip point
 T = {'x':c*1, 'y':0}
 
 # Calculating the lengths
 l_FT = distance(F,T)
 l_FM = distance(F,M)
-l_TM = distance(T,M)
+l_MT = distance(T,M)
 
 plotter(M, T)
 
+#angular variables lists
+
+list_alpha=[]
+list_gamma = []
+list_beta = []
+
 #bearing radius
-R = 0.1
+R = 1.8
+#-----------------------------------------------------------------------------#
+#Max strains
+max_strainFM = -(0.01*c)/l_FM
+max_strainMT = -(0.02*c)/l_MT
 
-#Max contractions
-max_contractionFM = 0.01*c
-max_contractionMT = 0.02*c
-
-list_contractionsFM = np.linspace(0, max_contractionFM, 2)
-list_contractionsMT = np.linspace(0, max_contractionMT, 2)
-for contractionFM in list_contractionsFM:
-    for contractionMT in list_contractionsMT:
+list_strainsFM = np.linspace(0, max_strainFM, 2)
+list_strainsMT = np.linspace(0, max_strainMT, 2)
+for strainFM in list_strainsFM:
+    alpha = -(strainFM)*l_FM/R
+    list_alpha.append(np.degrees(alpha))
+    
+    for strainMT in list_strainsMT:
         #in all interations the contration of the spring is the same
-        #calculating alpha, beta and gama based on SMA spring contraction
-        alpha = (contractionFM)/R
-        gamma = (contractionMT)/R
-     
+        #calculating alpha, beta and gama based on SMA spring strain
+        
+        gamma = -(strainMT)*l_MT/R
+        list_gamma.append(np.degrees(gamma))
+        
         M_alpha = {'x':M['x']*np.cos(alpha) - M['y']*np.sin(alpha),
            'y':M['x']*np.sin(alpha) + M['y']*np.cos(alpha)}
     
         beta = alpha + gamma
+        list_beta.append(np.degrees(beta))
+        
         T_alpha = {'x':T['x']*np.cos(alpha) - T['y']*np.sin(alpha),
            'y':T['x']*np.sin(alpha) + T['y']*np.cos(alpha)}
         
@@ -78,28 +90,32 @@ for contractionFM in list_contractionsFM:
                       (T_alpha['x']-M_alpha['x'])*np.sin(beta) + 
                       (T_alpha['y']-M_alpha['y'])*np.cos(beta)}
            
-        plotter(M_alpha, T_beta, fraction_M = contractionFM/max_contractionFM,
-                fraction_T = contractionMT/max_contractionMT)
+        plotter(M_alpha, T_beta, fraction_M = strainFM/max_strainFM,
+                fraction_T = strainMT/max_strainMT)
         print distance(F,M_alpha), distance(T_beta,M_alpha)
 plt.grid()
-plt.axes().set_aspect('equal')
 
-#TODO: change contraction to strain. Strain is negative when contracting and
-# positive when expanding. Usually strain = - contraction/length_o
+
+#DONE?#TODO: change strain to strain. Strain is negative when contracting and
+# positive when expanding. Usually strain = - strain/length_o
 #TODO: plots of wingtip displacement versus deflection (alpha,gamma)
-#TODO: plots of wingtip displacement versus strain (contractionFM, contractionMT)
+#TODO: plots of wingtip displacement versus strain (strainFM, strainMT)
 #TODO: plots of deflections (relative, alpha, gamma) versus strains
-    
+
+
+
+print  list_alpha
+print  list_beta
+print  list_gamma    
 '''   
     # Calculating the lengths
     l_FT=distance(F,T)
     l_FM=distance(F,M)
-    l_TM=distance(T,M)
+    l_MT=distance(T,M)
     print 'l_FM:', l_FM
     print "  "
 
 print 'l_FM:', l_FM
 
 '''
-
 
