156f1b291baeb30b05b849a33aab22ed7338bf7a
mecanon_experimental <leal2608@poli.ufrj.br>
2016-06-28 13:20:08 -0300

    arduino chematics and gyro-current code!
    Created schematics in fritzing and unified gyro and current codes, works
    like a charm.

--- arduino/gyro-current/gyro-current.ino
+++ arduino/gyro-current/gyro-current.ino
@@ -6,7 +6,7 @@ SparkFun Electronics 2011
 Ryan Owens
 3/16/11
 Updtaed for Arduino 1.0 and beyond:
-Joel Bartlett 
+Joel Bartlett
 SparkFun Electronics
 10/16/13
 This code is public domain buy you buy me a beer if you use this and we meet someday (Beerware license).
@@ -33,7 +33,7 @@ RunningMedian calibration_data_z = RunningMedian(100);
 //sensors current status.
 //To learn more about the registers on the ITG-3200, download and read the datasheet.
 char WHO_AM_I = 0x00;
-char SMPLRT_DIV= 0x15;
+char SMPLRT_DIV = 0x15;
 char DLPF_FS = 0x16;
 char GYRO_XOUT_H = 0x1D;
 char GYRO_XOUT_L = 0x1E;
@@ -46,11 +46,11 @@ char GYRO_ZOUT_L = 0x22;
 //DLPF, Full Scale Register Bits
 //FS_SEL must be set to 3 for proper operation
 //Set DLPF_CFG to 3 for 1kHz Fint and 42 Hz Low Pass Filter
-char DLPF_CFG_0 = (1<<0);
-char DLPF_CFG_1 = (1<<1);
-char DLPF_CFG_2 = (1<<2);
-char DLPF_FS_SEL_0 = (1<<3);
-char DLPF_FS_SEL_1 = (1<<4);
+char DLPF_CFG_0 = (1 << 0);
+char DLPF_CFG_1 = (1 << 1);
+char DLPF_CFG_2 = (1 << 2);
+char DLPF_FS_SEL_0 = (1 << 3);
+char DLPF_FS_SEL_1 = (1 << 4);
 
 //I2C devices each have an address. The address is defined in the datasheet for the device. The ITG-3200 breakout board can have different address depending on how
 //the jumper on top of the board is configured. By default, the jumper is connected to the VDD pin. When the jumper is connected to the VDD pin the I2C address
@@ -62,66 +62,83 @@ float x_cal, y_cal, z_cal;
 float x_angle = 0.;
 float y_angle = 0.;
 float z_angle = 0.;
-float calibration= 14.375;
-int step_time = 500;
- 
+float calibration = 14.375;
+int step_time = 50;
+
+const int transistorPin = 9;    // connected to the base of the transistor
+int i = 0;
+
 //In the setup section of the sketch the serial port will be configured, the i2c communication will be initialized, and the itg-3200 will be configured.
 void setup()
 {
+  // set  the transistor pin as output:
+  pinMode(transistorPin, OUTPUT);
   //Create a serial connection using a 9600bps baud rate.
   Serial.begin(9600);
-  
+
   //Initialize the I2C communication. This will set the Arduino up as the 'Master' device.
   Wire.begin();
-  
+
   //Read the WHO_AM_I register and print the result
-  char id=0; 
-  id = itgRead(itgAddress, 0x00);  
+  char id = 0;
+  id = itgRead(itgAddress, 0x00);
   Serial.print("ID: ");
   Serial.println(id, HEX);
-  
+
   //Configure the gyroscope
   //Set the gyroscope scale for the outputs to +/-2000 degrees per second
-  itgWrite(itgAddress, DLPF_FS, (DLPF_FS_SEL_0|DLPF_FS_SEL_1|DLPF_CFG_0));
+  itgWrite(itgAddress, DLPF_FS, (DLPF_FS_SEL_0 | DLPF_FS_SEL_1 | DLPF_CFG_0));
   //Set the sample rate to 100 hz
   itgWrite(itgAddress, SMPLRT_DIV, 9);
 
   //Calibrate values
 
-  for (int j = 0; j<20; j++){
+  for (int j = 0; j < 20; j++) {
     calibration_data_x.add(readX());
     calibration_data_y.add(readY());
     calibration_data_z.add(readZ());
     delay(step_time);
   }
-  x_cal = calibration_data_x.getAverage()/calibration;
-  y_cal = calibration_data_y.getAverage()/calibration;
-  z_cal = calibration_data_z.getAverage()/calibration;
-//  Serial.print("Calibrated values:");
-//  Serial.print(x_cal);
-//  Serial.print('\t');
-//  Serial.print(y_cal);
-//  Serial.print('\t');
-//  Serial.println(z_cal);
+  x_cal = calibration_data_x.getAverage() / calibration;
+  y_cal = calibration_data_y.getAverage() / calibration;
+  z_cal = calibration_data_z.getAverage() / calibration;
+  //  Serial.print("Calibrated values:");
+  //  Serial.print(x_cal);
+  //  Serial.print('\t');
+  //  Serial.print(y_cal);
+  //  Serial.print('\t');
+  //  Serial.println(z_cal);
   //Serial.print('\n');
 }
 
 //The loop section of the sketch will read the X,Y and Z output rates from the gyroscope and output them in the Serial Terminal
 void loop()
 {
+  //convert 0-360 angle to radian (needed for sin function)
+  float rad = DEG_TO_RAD * i;
+  //calculate sin of angle as number between 0 and 255
+  int sinOut = constrain((sin(rad) * 128) + 128, 0, 255);
+  // map the sensor value to a range from 0 - 255:
+  // int outputValue = map(sensorValue, 0, 1023, 0, 255);
+  // use that to control the transistor:
+  analogWrite(transistorPin, sinOut);
+  Serial.print(sinOut/255.);
+  Serial.print('\t');
+  i = i + 1;
+
   //Create variables to hold the output rates.
   float xRate, yRate, zRate;
 
   //Read the x,y and z output rates from the gyroscope.
-  xRate = readX()/calibration - x_cal;
-  yRate = readY()/calibration - y_cal;
-  zRate = readZ()/calibration - z_cal;
+  xRate = readX() / calibration - x_cal;
+  yRate = readY() / calibration - y_cal;
+  zRate = readZ() / calibration - z_cal;
 
-  x_angle = x_angle + xRate*step_time/1000.;
-  y_angle = y_angle + yRate*step_time/1000.;
-  z_angle = z_angle + zRate*step_time/1000.;
+  x_angle = x_angle + xRate * step_time / 1000.;
+  y_angle = y_angle + yRate * step_time / 1000.;
+  z_angle = z_angle + zRate * step_time / 1000.;
 
-  //Print the output rates to the terminal, seperated by a TAB character. 
+  //Print the output rates to the terminal, seperated by a TAB character.
   Serial.print(x_angle);
   Serial.print('\t');
   Serial.print(y_angle);
@@ -158,64 +175,64 @@ void itgWrite(char address, char registerAddress, char data)
 unsigned char itgRead(char address, char registerAddress)
 {
   //This variable will hold the contents read from the i2c device.
-  unsigned char data=0;
-  
+  unsigned char data = 0;
+
   //Send the register address to be read.
   Wire.beginTransmission(address);
   //Send the Register Address
   Wire.write(registerAddress);
   //End the communication sequence.
   Wire.endTransmission();
-  
+
   //Ask the I2C device for data
   Wire.beginTransmission(address);
   Wire.requestFrom(address, 1);
-  
+
   //Wait for a response from the I2C device
-  if(Wire.available()){
+  if (Wire.available()) {
     //Save the data sent from the I2C device
     data = Wire.read();
   }
-  
+
   //End the communication sequence.
   Wire.endTransmission();
-  
+
   //Return the data read during the operation
   return data;
 }
 
 //This function is used to read the X-Axis rate of the gyroscope. The function returns the ADC value from the Gyroscope
-//NOTE: This value is NOT in degrees per second. 
+//NOTE: This value is NOT in degrees per second.
 //Usage: int xRate = readX();
 int readX(void)
 {
-  int data=0;
-  data = itgRead(itgAddress, GYRO_XOUT_H)<<8;
-  data |= itgRead(itgAddress, GYRO_XOUT_L);  
-  
+  int data = 0;
+  data = itgRead(itgAddress, GYRO_XOUT_H) << 8;
+  data |= itgRead(itgAddress, GYRO_XOUT_L);
+
   return data;
 }
 
 //This function is used to read the Y-Axis rate of the gyroscope. The function returns the ADC value from the Gyroscope
-//NOTE: This value is NOT in degrees per second. 
+//NOTE: This value is NOT in degrees per second.
 //Usage: int yRate = readY();
 int readY(void)
 {
-  int data=0;
-  data = itgRead(itgAddress, GYRO_YOUT_H)<<8;
-  data |= itgRead(itgAddress, GYRO_YOUT_L);  
-  
+  int data = 0;
+  data = itgRead(itgAddress, GYRO_YOUT_H) << 8;
+  data |= itgRead(itgAddress, GYRO_YOUT_L);
+
   return data;
 }
 
 //This function is used to read the Z-Axis rate of the gyroscope. The function returns the ADC value from the Gyroscope
-//NOTE: This value is NOT in degrees per second. 
+//NOTE: This value is NOT in degrees per second.
 //Usage: int zRate = readZ();
 int readZ(void)
 {
-  int data=0;
-  data = itgRead(itgAddress, GYRO_ZOUT_H)<<8;
-  data |= itgRead(itgAddress, GYRO_ZOUT_L);  
-  
+  int data = 0;
+  data = itgRead(itgAddress, GYRO_ZOUT_H) << 8;
+  data |= itgRead(itgAddress, GYRO_ZOUT_L);
+
   return data;
 }
